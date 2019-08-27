73b6f83f462ceb29fd0a56c4adc80d3e0ed5b00c
前川 <qian41119122@gmail.com>
2019-08-28 00:05:31 +0900

    first commit

---
+++ shareSNS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shareSNS
+//
+//  Created by 前川 on 2019/08/28.
+//  Copyright © 2019 前川晃一郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
